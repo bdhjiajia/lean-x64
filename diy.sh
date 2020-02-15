7ff823e82deae24d02cfa3bb654768d763a6dd5b
luci-app-koolddns <bdhjiajia@gmail.com>
2020-02-15 14:09:03 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,10 +7,11 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i '/DISTRIB_DESCRIPTION/d' /etc/openwrt_release
+echo "DISTRIB_DESCRIPTION='OpenWrt Mod By RYJ '" >> /etc/openwrt_release
 git clone https://github.com/vernesong/OpenClash.git package/luci-app-openclash
 git clone https://github.com/AdguardTeam/AdGuardHome.git package/AdGuardHome
 git clone https://github.com/happyzhang1995/luci-app-adguardhome.git package/luci-app-adguardhome
 git clone https://github.com/happyzhang1995/openwrt-adguardhome.git package/openwrt-adguardhome
 ./scripts/feeds update -a
 ./scripts/feeds install -a
-
