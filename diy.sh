a365f5857cc4a32e261f3820399ae319a2bdba2e
luci-app-koolddns <bdhjiajia@gmail.com>
2020-02-15 19:06:37 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -10,11 +10,7 @@
 sed -i '/DISTRIB_DESCRIPTION/d' /etc/openwrt_release
 echo "DISTRIB_DESCRIPTION='OpenWrt Mod By RYJ '" >> /etc/openwrt_release
 sed -i '/REDIRECT --to-ports 53/d' /etc/firewall.user
-echo "iptables -t nat -A PREROUTING -p udp --dport 53 -j REDIRECT --to-ports 53" >> /etc/firewall.user
-echo "iptables -t nat -A PREROUTING -p tcp --dport 53 -j REDIRECT --to-ports 53" >> /etc/firewall.user
-echo "#单臂路由注释上2行，取消下行注释" >> /etc/firewall.user
 echo "#iptables -t nat -I POSTROUTING -j MASQUERADE" >> /etc/firewall.user
-#单独拉取软件包
 git clone https://github.com/vernesong/OpenClash.git package/luci-app-openclash
 git clone https://github.com/AdguardTeam/AdGuardHome.git package/AdGuardHome
 git clone https://github.com/happyzhang1995/luci-app-adguardhome.git package/luci-app-adguardhome
